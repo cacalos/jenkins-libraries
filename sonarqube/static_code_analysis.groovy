5fd604095d58d88974af451d5777960493b2383a
cacalos <cacalos@uangel.com>
2020-02-11 12:28:27 +0900

    change sonarqube groovy

--- sonarqube/static_code_analysis.groovy
+++ sonarqube/static_code_analysis.groovy
@@ -12,6 +12,7 @@ def call(){
   enforce = config.enforce_quality_gate ?:
             true
 
+  echo ${cred_id}
   stage("SonarQube Analysis"){
       withCredentials([usernamePassword(credentialsId: cred_id, passwordVariable: 'token', usernameVariable: 'user')]) {
         withSonarQubeEnv("SonarQube"){
