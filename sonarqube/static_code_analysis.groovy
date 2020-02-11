e803ffb117b273b4f52a12ebba750903cf17ecf7
cacalos <cacalos@uangel.com>
2020-02-11 16:13:43 +0900

    change sonarqube groovy

--- sonarqube/static_code_analysis.groovy
+++ sonarqube/static_code_analysis.groovy
@@ -32,7 +32,7 @@ def call(){
         }
         timeout(time: 1, unit: 'HOURS') {
 		  script {
-		    sleep 10
+		    sleep 1
             def qg = waitForQualityGate()
             if (qg.status != 'OK' && enforce) {
               error "Pipeline aborted due to quality gate failure: ${qg.status}"
